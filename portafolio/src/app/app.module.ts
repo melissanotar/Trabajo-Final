d4a46121fd07f20a844bb77bddcbbab45dfaa08a
Melissa <melissanotarfrancesco@gmail.com>
2023-08-05 14:46:16 -0300

    modificacion en componentes

--- portafolio/src/app/app.module.ts
+++ portafolio/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { EquipoComponent } from './components/equipo/equipo.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    BlogComponent,
+    EquipoComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
